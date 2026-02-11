276f2b7777a08b48dcc1833217d79481bbc56ac1
Frederik Born <macbook@MBPvonFrederik.fritz.box>
2026-02-11 13:23:26 +0100

    feat: add animated app logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import { motion } from "framer-motion";
+import mySportsLogo from '/public/images/AppLogo.png';
 
 function App() {
   const [sportsList, addSportsList] = useState([])
@@ -14,6 +16,7 @@ function App() {
     addSportsDuration(event.target.value)
   }
 
+
   const saveSportsUnit = () => {
     const sportsUnit = {
       id: Date.now(),
@@ -25,45 +28,46 @@ function App() {
     addSportsDuration("")
   }
 
-
-
-
   return (
+    <div className="app-container">
+      <div className="header">
+        <h1>Fitnesslog</h1>
+        <div className='shiny-logo'>
+        <motion.img
+          src={mySportsLogo}
+          width={120}
+          height={120}
+        />
+        </div>
+      </div>
 
 
-    <div>
-
-      <h1>Fitnesslog</h1>
-
-      <input
-        type='text'
-        placeholder='Sportart'
-        value={sportsType}
-        onChange={handleSportsTypeInput}
-      />
-
-      <input
-        type='number'
-        placeholder='Sportdauer'
-        value={sportsDuration}
-        onChange={handleSportsDurationInput}
-      />
-      <button onClick={saveSportsUnit}>Sporteinheit hinzufügen</button>
+      <div className="input-container">
+        <input
+          type='text'
+          placeholder='Sportart'
+          value={sportsType}
+          onChange={handleSportsTypeInput}
+        />
+        <input
+          type='number'
+          placeholder='Dauer in Minuten'
+          value={sportsDuration}
+          onChange={handleSportsDurationInput}
+        />
+        <button onClick={saveSportsUnit}>Sporteinheit hinzufügen</button>
+      </div>
 
 
-      <div>
+      <div className="list-container">
         {sportsList.map((item) => (
-          <div key={item.id}>
+          <div key={item.id} className="list-item">
             {item.type} : {item.duration} Minuten
           </div>
         ))}
       </div>
-
-
     </div>
   )
-
-
 }
 
 export default App
