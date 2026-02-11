1c6c91b81bc72de44c791a025f65a30643b43e6d
Frederik Born <macbook@MBPvonFrederik.fritz.box>
2026-02-11 12:27:22 +0100

    Add useState and input handlers for sports

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,47 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [sportsList, addSportsList] = useState([])
+  const [sportsType, addSportsType] = useState("")
+  const [sportsDuration, addSportsDuration] =useState("")
 
-  
+  const handleSportsTypeInput = (event) =>{
+    addSportsType(event.target.value)
+  }
+
+  const handleSportsDurationInput = (event) =>{
+    addSportsDuration(event.target.value)
+  }
+
+
+
+
+  return (
+
+
+    <div>
+
+      <input
+      type='text'
+      placeholder='Sportart'
+      onChange={handleSportsTypeInput}
+      />
+
+      <input
+      type='number'
+      placeholder='Sportdauer'
+      onChange={handleSportsDurationInput}
+      />
+
+
+
+
+
+
+    </div>
+  )
+
+
 }
 
 export default App
